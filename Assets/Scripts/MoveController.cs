c161e8316d55c776bcb6b5fa504fac5b1dab40f0
PasLelouch <wronggg@mail.ru>
2022-01-24 14:57:57 +0300

    Урок 6

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField] private float Speed;
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float Jumpspeed;
+    private bool groundedPlayer;
+    public bool isWalking;
+    public Animator anim;
+    Vector3 move;
+    float gravityValue = -9.81f;
+    AudioSource m_AudioSource;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+       
+
+
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && move.y < 0)
+        {
+            move.y = 0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+
+
+        move = transform.right * x + transform.forward * z;
+
+
+
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+
+            move.y += Mathf.Sqrt(Jumpspeed * -3.0f * gravityValue);
+
+        }
+        move.y += gravityValue * Time.deltaTime;
+        controller.Move(move * Speed * Time.deltaTime);
+
+
+    }
+
+    void FixedUpdate()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        anim.SetBool("IsWalking", isWalking);
+
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
+    }
+}
---
+++ Assets/Scripts/TargetPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TargetPlayer : MonoBehaviour
+{
+    public Transform _target;
+    private NavMeshAgent agent;
+    public UnityEvent Patrol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        _target = player.transform;
+        if (Vector3.Distance(agent.transform.position, player.transform.position) < 15)
+        {
+            agent.SetDestination(_target.position);
+        }
+        else
+        {
+            Patrol.Invoke();
+        }
+    }
+}
